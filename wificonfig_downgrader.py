4bfafb46a4dd9c062ccf0b92d15f35c882c370a9
Michael Hii <michaelhii@Michaels-MacBook-Air.local>
2021-03-30 00:23:03 -0700

    Parser and skeleton of code

--- wificonfig_downgrader.py
+++ wificonfig_downgrader.py
@@ -1,3 +1,37 @@
+import argparse
 import os
+import subprocess
+import sys
 import time
-import commands
+import xml.etree.ElementTree as ET
+
+
+def parse_xml(xml_path):
+    print("Parsing xml from {}".format(xml_path))
+
+parser = argparse.ArgumentParser(prog='wificonfig_downgrader',
+                                 usage='%(prog)s [options] inpath',
+                                 description='Convert WifiConfigStore.xml to wpa_supplicant.conf')
+parser.add_argument('inpath',
+                    metavar='inpath',
+                    type=str,
+                    help='File path to be converted')
+parser.add_argument('-o',
+                    '--outpath',
+                    metavar='outpath',
+                    type=str,
+                    default=os.getcwd(),
+                    help='Dir path to write file. Defaults to current working directory')
+
+args = parser.parse_args()
+
+input_path = args.inpath
+output_path = args.outpath
+
+print("Processing input file at {}".format(input_path))
+
+if not os.path.exists(input_path):
+    print('The file in the path specified does not exist')
+    sys.exit()
+
+parse_xml(input_path)
